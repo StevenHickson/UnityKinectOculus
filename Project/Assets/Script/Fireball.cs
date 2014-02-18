00c92ae9aff4f89a27e0997d10524eec050f37ca
Steven Hickson <me@stevenhickson.com>
2014-02-18 14:59:07 -0500

    Added Kinect and Fireball scripts

---
+++ Project/Assets/Script/Fireball.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+	
+	public Rigidbody Projectile;
+	public GameObject HandPosition;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+            Rigidbody clone;
+            clone = (Rigidbody)Instantiate(Projectile, HandPosition.transform.position, transform.rotation);
+            clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+        }
+	}
+}
