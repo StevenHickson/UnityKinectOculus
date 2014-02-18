f97fdd60f57852793cf60dcad924b83dee2b1340
Steven Hickson <me@stevenhickson.com>
2014-02-18 18:26:02 -0500

    Got fireball working. Next add velocity parameters based on hand speed

--- Project/Assets/Script/Fireball.cs
+++ Project/Assets/Script/Fireball.cs
@@ -5,17 +5,28 @@ public class Fireball : MonoBehaviour {
 	
 	public Rigidbody Projectile;
 	public GameObject HandPosition;
+	
+	private Vector3 lastPos;
+	private float THRESH, MAX_THRESH, SPEED;
 	// Use this for initialization
 	void Start () {
-	
+		lastPos = HandPosition.transform.position;
+		THRESH = 0.11f;
+		MAX_THRESH = 0.4f;
+		SPEED = 3;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Fire1")) {
+		float dist = (HandPosition.transform.position - lastPos).magnitude;
+		print ("Distance: " + dist);
+		if (Input.GetButtonDown("Fire1") || (dist > THRESH && dist < MAX_THRESH)) {
             Rigidbody clone;
-            clone = (Rigidbody)Instantiate(Projectile, HandPosition.transform.position, transform.rotation);
-            clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+            clone = (Rigidbody)Instantiate(Projectile, HandPosition.transform.position, HandPosition.transform.rotation);
+            clone.velocity = transform.TransformDirection(Vector3.forward * SPEED);
         }
+		
+		lastPos = HandPosition.transform.position;
+		
 	}
 }
